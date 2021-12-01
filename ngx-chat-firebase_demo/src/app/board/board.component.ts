617396a96e52badcf10d7d2d755f8d681fc3fb73
redpeaks33 <redpeaks33@gmail.com>
2021-12-01 11:34:49 +0900

    update

--- ngx-chat-firebase_demo/src/app/board/board.component.ts
+++ ngx-chat-firebase_demo/src/app/board/board.component.ts
@@ -6,10 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./board.component.css']
 })
 export class BoardComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
